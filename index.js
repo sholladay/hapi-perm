d6ac426b30bcd4f628079e7900041aeced18fbd3
Seth Holladay <me@seth-holladay.com>
2019-01-04 00:47:36 -0500

    Create new connection if none are open in the pool

--- index.js
+++ index.js
@@ -18,24 +18,22 @@ const createConnection = (config) => {
 const register = async (server, option) => {
     const pool = new Set();
     const getConnection = async () => {
-        // TODO: Implement max connections ceiling and if it is reached, wait for a busy
-        // connection to become available, instead of creating a new connection.
-        if (pool.size === 0) {
-            const connection = await createConnection(option);
-            const deleteConnection = () => {
-                pool.delete(connection);
-            };
-            connection.once('close', deleteConnection);
-            connection.once('timeout', deleteConnection);
-            connection.once('error', deleteConnection);
-            return connection;
-        }
         for (const connection of pool) {
             pool.delete(connection);
             if (connection.isOpen()) {
                 return connection;
             }
         }
+        // TODO: What if there are many outstanding connections not in the pool?
+        //       Maybe track those and implement a max connections ceiling.
+        const connection = await createConnection(option);
+        const deleteConnection = () => {
+            pool.delete(connection);
+        };
+        connection.once('close', deleteConnection);
+        connection.once('timeout', deleteConnection);
+        connection.once('error', deleteConnection);
+        return connection;
     };
 
     pool.add(await getConnection());
@@ -43,12 +41,9 @@ const register = async (server, option) => {
     server.decorate('server', 'db', async (query) => {
         const connection = await getConnection();
         const result = await query.run(connection);
-        // TODO: If result is a cursor, we should probably wait for it to close before
-        // adding the connection back to the pool.
         pool.add(connection);
         return result;
     });
-    // TODO: Find some way to get rid of this in favor of just server.db()
     server.decorate('server', 'dbCursor', async (query) => {
         const connection = await getConnection();
         const cursor = await query.run(connection);
