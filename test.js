07fc56556083b387f43f13314d2478514d2889bc
Seth Holladay <me@seth-holladay.com>
2019-01-04 00:12:25 -0500

    Add actual database tests

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import test from 'ava';
 import hapi from 'hapi';
+import r from 'rethinkdb';
+import { init, cleanup } from 'thinkable';
 import perm from '.';
 
 const makeRoute = (option) => {
@@ -29,6 +31,20 @@ const makeServer = async (option) => {
     return server;
 };
 
+const jamie = {
+    id   : 1,
+    name : 'jamie'
+};
+
+const seed = {
+    unicorns : [jamie]
+};
+
+const dbName = 'test';
+
+test.before(init({ [dbName] : seed }));
+test.after.always(cleanup);
+
 test('without perm', async (t) => {
     const server = await makeServer({
         plugin : null
@@ -43,6 +59,33 @@ test('without perm', async (t) => {
 });
 
 test('error if unable to connect', async (t) => {
-    const err = await t.throwsAsync(makeServer());
-    t.is(err.message, 'Unable to reach RethinkDB at localhost:28015');
+    const err = await t.throwsAsync(makeServer({
+        plugin : {
+            plugin  : perm,
+            options : {
+                port : 1234
+            }
+        }
+    }));
+    t.is(err.message, 'Unable to reach RethinkDB at localhost:1234');
+});
+
+test('server.db() runs a query and returns a document', async (t) => {
+    const server = await makeServer();
+    const doc = await server.db(r.table('unicorns').filter({ name : 'jamie' }).nth(0));
+    t.deepEqual(doc, jamie);
+});
+
+test('server.db() runs a query and returns a cursor', async (t) => {
+    const server = await makeServer();
+    const cursor = await server.db(r.table('unicorns'));
+    const unicorns = await cursor.toArray();
+    t.deepEqual(unicorns, seed.unicorns);
+});
+test('server.dbCursor() runs a query and returns a cursor and done callback', async (t) => {
+    const server = await makeServer();
+    const { cursor, done } = await server.dbCursor(r.table('unicorns'));
+    const unicorns = await cursor.toArray();
+    t.notThrows(done);
+    t.deepEqual(unicorns, seed.unicorns);
 });
